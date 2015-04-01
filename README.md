8c40fd60bf4c961b1147aa546392f5d18d11870d
sole <sole@mozilla.com>
2015-04-01 18:51:32 +0100

    showing basic info

---
+++ README.md
@@ -0,0 +1,5 @@
+# network info
+
+This is a certified app showing how to find info about the current wifi connection in Firefox OS.
+
+It's very simple and it's more of an exercise for me to learn than a very good example of how this should be done probably. Please excuse any weird thing here :-)
---
+++ js/app.js
@@ -0,0 +1,36 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+ 'use strict';
+
+  var info = document.getElementById('info');
+  var wifiManager = navigator.mozWifiManager;
+
+  if(!wifiManager) {
+    log('WifiManager not available');
+  } else {
+    
+    var status = wifiManager.connection.status;
+    var connInfo = wifiManager.connectionInformation;
+    var fields = [];
+    fields.push(['mac', wifiManager.macAddress]);
+
+    if(status === 'associated' || status === 'connected') {
+      var network = wifiManager.connection.network;
+      fields.push(['SSID', network.ssid]);
+    }
+
+    if(connInfo) {
+      fields.push(['ip', connInfo.ipAddress]);
+    }
+    
+    var txt = fields.map(function(pair) {
+      return `<strong>${pair[0]}: </strong> ${pair[1]}`;
+    }).join('<br />');
+    log(txt);
+  }
+
+  function log(txt) {
+    info.innerHTML += txt + '<br />';
+  }
+
+});
